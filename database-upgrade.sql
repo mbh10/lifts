59fec9c5a205b6e7fc911506b5cb888afe61ea4c
mbh10 <michael.hammontree@gmail.com>
2026-08-03 20:39:06 -0500

    Delete database-upgrade.sql

--- database-upgrade.sql
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://hncumgnqselkadhgihzi.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_bbo32iypA0wy7Ej3QjRDVA_rxVue_CF';
-export const APP_URL = 'https://mbh10.github.io/lifts/';
